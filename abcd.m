f94c234ae53f8194ba3f8f1a19dfe5a03a43dcf0
WYJ <wyjunnn@163.com>
2022-04-02 07:47:30 +0800

    rename

--- abcd.m
+++ abcd.m
@@ -3,7 +3,7 @@ function [delta_xi,EMF]  = zhiqianChen(g1,g2,omega_e,R_s,Ld,Lq,xi,u_alphabeta,i_
     EMF = [0;0];
     %p¦Î=    A_0 ¦Î   +   A_1 i   +   A_2 u
     %A_0=   G/L_d+¦Ø_re  J 
-    %A_1=   G¦Ø_re  J+G(R_s-?L¦Ø_re J+G)/L_d
+    %A_1=   ¦Ø_re  JG+G(R_s-?L¦Ø_re J+G)/L_d
     %A_2=   - G/L_d
     G = [g1 -g2;
         g2 g1];
@@ -13,7 +13,7 @@ function [delta_xi,EMF]  = zhiqianChen(g1,g2,omega_e,R_s,Ld,Lq,xi,u_alphabeta,i_
         1 0];
     
     A_0 = G/Ld + omega_e*J;
-    A_1 = G*omega_e*J + G*(R_s-(Ld-Lq)*omega_e*J +G)/Ld;
+    A_1 = omega_e*J*G + G*(R_s-(Ld-Lq)*omega_e*J +G)/Ld;
     A_2 = -G/Ld;
     delta_xi = A_0*xi + A_1*i_alphabeta + A_2*u_alphabeta;
     EMF = xi + G*i_alphabeta;
